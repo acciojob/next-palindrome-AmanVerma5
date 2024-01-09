a76d2adb714faf38d01657a8265064250319287b
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2024-01-09 14:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function nextPalindrome(num) {
   //your JS code here.
+	let number=parseInt(num)+1;
+
+	while(!isPallindrome(number.toString())){
+		number=number+1;
+	}
+	return number;
+}
+function isPallindrome(str){
+	return str===str.split('').reverse().join('');
 }
 
 const input = prompt("Enter a palindrome number");
